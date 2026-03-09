fc274e599f3c41524532053dd729be2fecd70274
KimGyeonkang <v.tang3203@gmail.com>
2026-03-09 11:22:17 +0900

    About 정보 업데이트(used_api -> 'REST API' 추가) (260309)

--- src/routes/About.js
+++ src/routes/About.js
@@ -14,7 +14,7 @@ function About(props) {
             <p className="used_technologies">Used Technologies: React, JS, HTML, CSS, VScode, Git, GitHub
                 (まずは、JS,HTML,CSSの知識はほとんどないまま、Reactを活用し模写コーディングしてみました)
             </p>
-            <p className="used_api">Used Api: https://api.annict.com</p>
+            <p className="used_api">Used Api: https://api.annict.com (REST API)</p>
             <p className="page_introduction">普段アニメ鑑賞が好きな者として、自分のアニメ紹介ページをずっと作ってみたいと思いました。
                 その中で、ちょうどウェブ開発に興味を持つことになり、「足りなくてもまずは自分でページをつくってみよう！」と考え、披露しています！
             </p>
